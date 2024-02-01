c99920fc2fd9ecc9d9b4fc869234b33360f6ea5f
EivindLillefosse <eivind.lillefosse@gmail.com>
2024-02-01 14:46:10 +0100

    Update screen dimensions and button layout in MainGUI.py

--- MainGUI.py
+++ MainGUI.py
@@ -3,8 +3,8 @@
 import tkinter as tk
 import tkinter.font as tkFont
 
-screen_width = 1500
-screen_height = 800
+screen_width = 1080
+screen_height = 1920
 padding = 10
 
 
@@ -13,6 +13,7 @@ root = tk.Tk()
 root.title("Sengepost")
 # Set geometry (widthxheight)
 root.geometry(f'{screen_width+110}x{screen_height}')
+
 
 #add image file
 pixelVirtual = tk.PhotoImage(width=1, height=1)
@@ -23,19 +24,33 @@ returnIm = returnIm.subsample(2, 2)
 def MainMenu():
     for widget in root.winfo_children():
         widget.destroy()
+    # Create three big buttons
     b1 = tk.Button(root, text = "Button 1", bg = "red", fg = "white", image=pixelVirtual ,compound="c", height = 200, width = int(screen_width/3))
     b2 = tk.Button(root, text = "Vann", bg = "blue",fg = "white", image=pixelVirtual ,compound="c", command = Vann, height = 200, width = int(screen_width/3))
     b3 = tk.Button(root, text = "Button 3", bg = "blue", fg = "white", image=pixelVirtual ,compound="c", command = lambda: print("Hello World"), height = 200, width = int(screen_width/3)) 
+    b1.grid(row = 1, column = 1, padx=padding, pady=padding)
+    b2.grid(row = 1, column = 2, padx=padding, pady=padding)
+    b3.grid(row = 1, column = 3, padx=padding, pady=padding)
+    # Create return btn
+    createReturnBtn()
     return
 def createReturnBtn():
     returnBtn = tk.Button(root, bg = "white", fg = "black", image=returnIm ,compound="c", command = MainMenu, width = int(screen_width/3))
-    returnBtn.grid(row = 3, column = 3, padx=padding, pady=padding)
+    # Setting the position of the button on the bottom of the screen.
+    returnBtn.place(x = screen_width- int(screen_width/3) +100, y = screen_height-270)
+    
     return
 def Vann():
+    
     for widget in root.winfo_children():
         widget.destroy()
-    vannHøy = tk.Button(root, text = "Stor hastegrad", bg = "red", fg = "white", image=pixelVirtual ,compound="c", height = 200, width = int(screen_width/3))
-    vannLav = tk.Button(root, text = "Liten hastegrad", bg = "green",fg = "white", image=pixelVirtual ,compound="c", command = lambda: print("Hello World"), height = 200, width = int(screen_width/3))
+    # Create a title over grid
+    title = tk.Label(root, text="Vann", font=("Helvetica", 16))
+    # Center title
+    title.grid(row = 0, column = 1, columnspan = 4)
+    # Create two buttons
+    vannHøy = tk.Button(root, text = "Stor hastegrad", bg = "red", fg = "white", image=pixelVirtual ,compound="c", height = 200, width = int(screen_width/2))
+    vannLav = tk.Button(root, text = "Liten hastegrad", bg = "green",fg = "white", image=pixelVirtual ,compound="c", command = lambda: print("Hello World"), height = 200, width = int(screen_width/2))
     vannHøy.grid(row = 1, column = 1,columnspan= 2, padx=padding, pady=padding)
     vannLav.grid(row = 1, column = 3,columnspan=2, padx=padding, pady=padding)
     createReturnBtn()
@@ -43,18 +58,7 @@ def Vann():
     return
 
 # Create three big buttons 
-b1 = tk.Button(root, text = "Button 1", bg = "red", fg = "white", image=pixelVirtual ,compound="c", height = 200, width = int(screen_width/3))
-b2 = tk.Button(root, text = "Vann", bg = "blue",fg = "white", image=pixelVirtual ,compound="c", command = Vann, height = 200, width = int(screen_width/3))
-b3 = tk.Button(root, text = "Button 3", bg = "blue", fg = "white", image=pixelVirtual ,compound="c", command = lambda: print("Hello World"), height = 200, width = int(screen_width/3))
-# Position buttons with grid layout
-title = tk.Label(root, text = "Sengepost", font = ("Helvetica", 50))
-title.grid(row = 0, column = 0, columnspan = 4)
-b1.grid(row = 1, column = 1, padx=padding, pady=padding)
-b2.grid(row = 1, column = 2, padx=padding, pady=padding)
-b3.grid(row = 1, column = 3, padx=padding, pady=padding)
-# Create return btn
-returnBtn = tk.Button(root, bg = "white", fg = "black", image=returnIm ,compound="c", command = MainMenu, width = int(screen_width/3))
-returnBtn.grid(row = 3, column = 3, padx=padding, pady=padding)
+MainMenu()
 
 # Start the GUI
 
